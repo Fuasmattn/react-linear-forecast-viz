3810f132bd52dd914dda8e1c2eb723b87f9b0369
Martin Prinz <martin.prinz@maibornwolff.de>
2018-09-21 16:37:13 +0200

    tooltips

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     super(props);
     this.state = {
       data : [{
-        id: 0,
+        id: 'HSJKS 123XY',
         startDate: new Date('05 October 2018 14:48 UTC'),
         endDate: new Date('29 October 2018 14:48 UTC'),
         startMileage: 2000,
@@ -17,7 +17,7 @@ class App extends Component {
         today:  new Date('17 October 2018 14:48 UTC')
       },
       {
-        id: 1,
+        id: 'ISOA 829XY',
         startDate: new Date('07 October 2018 14:48 UTC'),
         endDate: new Date('23 October 2018 14:48 UTC'),
         startMileage: 3000,
@@ -26,7 +26,7 @@ class App extends Component {
         today:  new Date('18 October 2018 11:48 UTC')
       },
       {
-        id: 2,
+        id: 'OUSA 231GH',
         startDate: new Date('07 October 2018 14:48 UTC'),
         endDate: new Date('23 October 2018 14:48 UTC'),
         startMileage: 1000,
@@ -35,7 +35,7 @@ class App extends Component {
         today: new Date('22 October 2018 14:48 UTC')
       },
       {
-        id: 3,
+        id: 'SSKO 933JD',
         startDate: new Date('02 October 2018 14:48 UTC'),
         endDate: new Date('05 October 2018 14:48 UTC'),
         startMileage: 1000,
@@ -54,7 +54,7 @@ class App extends Component {
           size={{width: 800, height: 400}}
           color={'#0000cc'}
           thickness={3}
-          circleRadius={5}
+          circleRadius={0}
           xAxisText='Time progression (%)'
           yAxisText='Mileage progression (%)'
         ></PredictionLines>
--- src/PredictionLines.js
+++ src/PredictionLines.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { select, axisBottom, axisLeft, scaleLinear } from 'd3';
+import { select, event,hsl, axisBottom, axisLeft, scaleLinear } from 'd3';
 import { Util } from './Util';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 const PADDING = 20;
+const PADDING_2 = PADDING * 2;
 
 class PredictionLines extends Component {
  constructor(props) {
@@ -20,11 +22,13 @@ class PredictionLines extends Component {
  }
 
 componentDidMount() {
- this.setupViz();
+ this.doViz();
 }
 
-setupViz() {
+doViz() {
  const { data, size, color, thickness, circleRadius, xAxisText, yAxisText } = this.state;
+ const primaryColor = hsl(color);
+ const darkerColor = primaryColor.darker(1);
 
  // percentage of how much of the maximum mileage is already reached
  // const yMin = min(data, d => d.currentMileage / d.endMileage);
@@ -36,32 +40,40 @@ setupViz() {
  const svg = select('svg');
  
  const xScaleAxis = scaleLinear().domain([0, 100]).range([PADDING, size.width - 80 ]).clamp(true);
- const yScaleAxis = scaleLinear().domain([100, 0]).range([PADDING, size.height - PADDING * 2]).clamp(true);
+ const yScaleAxis = scaleLinear().domain([100, 0]).range([PADDING, size.height - PADDING_2]).clamp(true);
  
  const xScale = scaleLinear().domain([0, 100]).range([PADDING, size.width - PADDING / 2]).clamp(true);
- const yScale = scaleLinear().domain([100, 0]).range([PADDING, size.height - PADDING * 2]).clamp(true);
+ const yScale = scaleLinear().domain([100, 0]).range([PADDING, size.height - PADDING_2]).clamp(true);
  
  const xAxis = axisBottom().scale(xScaleAxis);
  const yAxis = axisLeft().scale(yScaleAxis);
 
  // rendering
- svg.append('g').attr('transform', `translate(60, ${size.height - PADDING * 2})`).call(xAxis);
+ svg.append('g').attr('transform', `translate(60, ${size.height - PADDING_2})`).call(xAxis);
  svg.append('g').attr('transform', 'translate(80, 0)').call(yAxis);
+
+  // tooltip
+  const tooltip = select('body').append('div')	
+    .attr('class', 'tooltip')				
+    .style('opacity', 0);
+
 
  // axes description
  svg.append('text')
  .attr('text-anchor', 'middle')
- .attr('transform', `translate(20, ${size.height / 2})rotate(-90)`)
+ .style('font-size', 12)
+ .attr('transform', `translate(${PADDING_2}, ${size.height / 2})rotate(-90)`)
  .text(yAxisText);
 
 svg.append('text')
  .attr('text-anchor', 'middle')
+ .style('font-size', 12)
  .attr('transform', `translate(${size.width / 2}, ${size.height - 5})`)
  .text(xAxisText);
 
  const lines = svg.selectAll('line').data(data, d => d);
  const circles = svg.selectAll('circles').data(data, d => d);
- const zero = { x: 80, y: size.height - PADDING * 2 };
+ const zero = { x: 80, y: size.height - PADDING_2 };
 
  lines.enter()
   .append('line')
@@ -69,8 +81,11 @@ svg.append('text')
   .attr('y1', d => yScale((d.startMileage / d.endMileage) * 100))
   .attr('x2', d => xScale(Util.timeProgress(d) * 100))
   .attr('y2', d => yScale((d.currentMileage / d.endMileage) * 100))
-  .attr('stroke', color)
-  .attr('stroke-width', thickness);
+  .attr('stroke', primaryColor)
+  .attr('opacity', .5)
+  .attr('stroke-width', thickness)
+  .on('mouseover', this.handleMouseOver)
+  .on('mouseout', this.handleMouseOut);
 
 
   // forecast lines
@@ -86,21 +101,41 @@ svg.append('text')
   )))
   .attr('stroke', 'black')
   .attr('opacity', .2)
-  .attr('stroke-width', thickness);
+  .attr('stroke-width', thickness / 2);
 
   circles.enter()
    .append('circle')
    .attr('cx', d => xScale(Util.timeProgress(d) * 100))
    .attr('cy', d => yScale((d.currentMileage / d.endMileage) * 100))
    .attr('r', circleRadius)
-   .attr('fill', color)
+   .attr('fill', darkerColor)
   
 }
 
-slopeToPoint = (slope, startMileage) => {
-console.log(slope, startMileage)
- return (slope + startMileage) * 100;
+handleMouseOver = function(d, i) {
+  select(this).attr('opacity', 1);
+  const tooltip = select('.tooltip')
+  tooltip.transition()		
+    .duration(200)		
+    .style('opacity', .9);		
+  tooltip.html(
+    `<p><strong>${d.id}</strong></p>
+ <p>Mileage: ${d.currentMileage}/${d.endMileage}km</p>
+ <p>Start Mileage: ${d.startMileage}km</p>
+ <p>End Date: ${moment(d.endDate).format('DD.MM.YYYY')}</p>
+ `)	
+    .style('left', (event.pageX + 20) + 'px')		
+    .style('top', (event.pageY - 50) + 'px');			
 }
+handleMouseOut = function(d, i) {
+  select(this).attr('opacity', .5);
+  const tooltip = select('.tooltip')
+  tooltip.transition()		
+    .duration(500)		
+    .style('opacity', 0);	
+}
+
+slopeToPoint = (slope, startMileage) => (slope + startMileage) * 100;
 
  render() {
   const { width, height } = this.state.size;
